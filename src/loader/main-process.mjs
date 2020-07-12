0c6b08617026ab150336d48f58bb4b84805fedd2
Phil Toms <phil.toms@hotmail.co.uk>
2020-07-12 14:58:55 +0100

    refactor(loader): worker api

--- src/loader/main-process.mjs
+++ src/loader/main-process.mjs
@@ -4,40 +4,38 @@ import path, { dirname } from 'path';
 import se from 'serialize-error';
 
 import cache from 'iep-cache';
+import pubsub from 'iep-pubsub';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const workerService = path.resolve(__dirname, 'child-process.mjs');
+const childProcess = path.resolve(__dirname, 'child-process.mjs');
 const loaderHooks = path.resolve(__dirname, 'loader-hooks.mjs');
 
 const serviceMap = {};
-const callbacks = [];
-const pubsub = {
-  send: (message) =>
-    Object.values(serviceMap).forEach((worker) => worker.send(message)),
-  on: (message, cb) => callbacks.push(cb),
-};
+
+const { publish, once } = pubsub();
 
 export default ({ iep: { serverEntry }, 'iep-cache': conf }) => {
-  const iepMap = cache('iepMap', { ...conf, pubsub });
-  const iepSrc = cache('iepSrc', { ...conf, pubsub });
+  const iepMap = cache('iepMap', conf);
+  const iepSrc = cache('iepSrc', conf);
 
-  let workerSequenceNo = 1;
+  let requestSequenceNo = 1;
   return [
     iepMap,
     iepSrc,
     (ticket, buffer) => {
-      const worker = serviceMap[ticket] || restart(ticket, conf);
-      const requestId = workerSequenceNo++;
-      worker.send({
+      const child = serviceMap[ticket] || worker(ticket, conf);
+      const { publish } = pubsub(child);
+      const requestId = requestSequenceNo++;
+      publish('render', {
         ticket,
         serverEntry,
         buffer,
         requestId,
       });
       return new Promise((resolve, reject) => {
-        worker.on('message', ({ responseId, buffer, err }) => {
+        once('render', (channel, { responseId, buffer, err }) => {
           if (err) {
             return reject(se.deserializeError(JSON.parse(err)));
           }
@@ -50,13 +48,16 @@ export default ({ iep: { serverEntry }, 'iep-cache': conf }) => {
   ];
 };
 
-export const restart = (ticket, conf) => {
+export const restart = (ticket) => {
   if (serviceMap[ticket]) {
     serviceMap[ticket].kill();
+    Reflect.deleteProperty(serviceMap, ticket);
   }
+};
 
-  const worker = fork(
-    workerService,
+const worker = (ticket, conf) => {
+  const child = fork(
+    childProcess,
     [
       `--cache-lazy-load=true`,
       `--cache-persist-url=${conf['cache-persist-url']}`,
@@ -67,17 +68,19 @@ export const restart = (ticket, conf) => {
         '--experimental-loader',
         loaderHooks,
         '--experimental-specifier-resolution=node',
+        // '--trace-warnings',
         // '--no-warnings',
-        '--inspect-brk=localhost:9222',
+        // '--inspect-brk=localhost:9222',
       ],
     }
   );
-  worker.on('message', (message) => {
-    callbacks.forEach((cb) => cb(message));
-    if (message.entity) {
-      workers.send(message);
-    }
-  });
 
-  return (serviceMap[ticket] = worker);
+  const { subscribe } = pubsub(child);
+
+  // broadcast to all siblings
+  subscribe('render', publish);
+  subscribe('iepMap', publish);
+  subscribe('iepSrc', publish);
+
+  return (serviceMap[ticket] = child);
 };
--- src/loader/worker.mjs
+++ src/loader/worker.mjs
@@ -1,25 +1,32 @@
 import se from 'serialize-error';
+import pubsub from 'iep-pubsub';
 
-export default (pubsub) => {
-  pubsub.on('message', (message) => {
-    const { ticket, serverEntry, buffer, requestId } = message;
-    if (requestId) {
-      const specifier = `${serverEntry}${
-        serverEntry.includes('?') ? '&' : '?'
-      }__iep=${ticket}`;
-      return import(specifier)
-        .then((app) => {
-          pubsub.send({
-            responseId: requestId,
-            buffer: (app.default || app)(buffer),
-          });
-        })
-        .catch((err) => {
-          pubsub.send({
-            responseId: requestId,
-            err: JSON.stringify(se.serializeError(err)),
-          });
+const render = (publish) => (
+  channel,
+  { ticket, serverEntry, buffer, requestId }
+) => {
+  if (requestId) {
+    const specifier = `${serverEntry}${
+      serverEntry.includes('?') ? '&' : '?'
+    }__iep=${ticket}`;
+
+    return import(specifier)
+      .then((app) => {
+        publish('render', {
+          responseId: requestId,
+          buffer: (app.default || app)(buffer),
         });
-    }
-  });
+      })
+      .catch((err) => {
+        publish('render', {
+          responseId: requestId,
+          err: JSON.stringify(se.serializeError(err)),
+        });
+      });
+  }
+};
+
+export default (process) => {
+  const { publish, subscribe } = pubsub(process);
+  subscribe('render', render(publish));
 };
